2249c96e3d4e7a78c6d7a7651969c906a823deb6
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-04-26 18:10:56 +0100

    short video

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -68,52 +68,24 @@ const Landing = () => {
         <Swiper getSwiper={setSwiper}>
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage1})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Оформи красиво свой онлайн бизнес</h1>
-              <h2 className="main-page__page1__subtitle">Легко, просто и быстро создай свое модное приложение без дизайнеров и вэб мастеров. 
-              Получай больше обращений и продаж через  свои соцсети.</h2>
+              <h1 className="main-page__page1__header">Оформи красиво ссылку<br />в профиле своего<br />Инстаграм!</h1>
             </div>
           </div>
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage2})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Для блондинок,<br /> но не только...</h1>
-              <h2 className="main-page__page1__subtitle">Мы создали очень простой интерфейс, который позволяет создавать свое 
-              сайт и приложение за 5 минут.</h2>
+              <h1 className="main-page__page1__header">Для &laquo;Блондинок&raquo;,<br />без регистрации,<br />бесплатно и красиво!</h1>
             </div>
           </div>
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage3})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Добавляй свои контакты</h1>
-              <h2 className="main-page__page1__subtitle">Instagram позволяет разместить только одну ссылку. Добавляйте все мессенджеры,
-              e-mail и телефон для связи в один клик.</h2>
+              <h1 className="main-page__page1__header">Добавь мессенджеры,<br />соцсети, YouTube и...<br />все, что угодно!</h1>
             </div>
           </div>
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage4})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Создавай креативы</h1>
-              <h2 className="main-page__page1__subtitle">Переключайся между готовыми темами и добавляй ссылки на лучший контент.
-              Используйте автоматизированный импорт из Youtube и Instagram.</h2>
-            </div>
-          </div>
-          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage5})` }}>
-            <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Повышай охваты</h1>
-              <h2 className="main-page__page1__subtitle">Размещай ссылки на эту страницу в постах и описании профиля. На тарифе "Blogger"
-              получай трафик из поиска Google.</h2>
-            </div>
-          </div>
-          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage6})` }}>
-            <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Оставайся на связи</h1>
-              <h2 className="main-page__page1__subtitle">Друзья cмогут в один клик сохранить твое приложение на своем
-              телефоне, а потом поделиться им. Разве это не круто, оставаться на экране телефона у своих друзей и заказчиков?</h2>
-            </div>
-          </div>
-          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage7})` }}>
-            <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Что такое Sweety?</h1>
-              <h2 className="main-page__page1__subtitle">Sweety - это очень простой конструктор микро-целевых страниц для ваших соцсетей, чтобы 
-              вы получали больше обращений через ваши мессенджеры. Используйте БЕЗ РЕГИСТРАЦИИ, бесплатно, навсегда.</h2>
-               <div className="main-page__page1__container2">
+              <h1 className="main-page__page1__header">Посмотри, как микроблогер из Инстаграм использует Sweety</h1>
+              <h2 className="main-page__page1__subtitle" >Пример: <a href="https://sweety.link/vika" target="_blank">sweety.link/vika</a></h2>
+              <div className="main-page__page1__container2">
                 <div className="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/zsy0pbMBBRI?controls=0" 
                 frameborder="0" rel="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
@@ -125,16 +97,13 @@ const Landing = () => {
             <Loading />
           </div>
         </Swiper>
-        {!startOpened && currentPage < 7 && (
+        {!startOpened && currentPage < 4 && (
           <div className="main-page__footer">
             <div className="main-page__pages">
               <div className={classnames({ 'main-page__pages__selected': currentPage === 0 })} />
               <div className={classnames({ 'main-page__pages__selected': currentPage === 1 })} />
               <div className={classnames({ 'main-page__pages__selected': currentPage === 2 })} />
               <div className={classnames({ 'main-page__pages__selected': currentPage === 3 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 4 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 5 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 6 })} />
             </div>
             <Button
               noStyled
