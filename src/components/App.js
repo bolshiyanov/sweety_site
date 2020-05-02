61b2b58841237394f43d26ce84984677c8b711cf
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-05-02 19:46:25 +0000

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,7 @@ const App = () => {
       </Helmet>
       <div className="app" style={backgroundStyles}>
         { GoogleAnalytics.init() && <GoogleAnalytics.RouteTracker /> }
-        <div className="app-container" style={backgroundStyles}>
+        <div className="app-container">
           <Header
             name={data.name}
             avatar={data.avatar}
