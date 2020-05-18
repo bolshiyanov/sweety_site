5fd9b8e09dd8d9d1931bf2381561baa2276d589a
test <test@user.com>
2020-05-18 11:50:45 +0300

    Fix youtube playlist

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,5 +1,5 @@
 export const getSearchString = (search, variable) => {
-  const query = search.substring(1);
+  const query = search.substring(search.indexOf("?") + 1);
   const vars = query.split('&');
   for (let i = 0; i < vars.length; i++) {
     const pair = vars[i].split('=');
