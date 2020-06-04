883455bd6ab45b1de6b8af3be9986712d809efd4
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-06-04 10:52:03 +0100

    with out links in the popup

--- src/components/PwaInstallIOs/index.js
+++ src/components/PwaInstallIOs/index.js
@@ -5,7 +5,6 @@ import PwaInstallPopupIOS from 'react-pwa-install-ios';
 import './index.scss';
 
 const PwaInstallIOs = ({profile}) => {
-  const { url } = useSelector((state) => state.config.data);
   return (
     <PwaInstallPopupIOS delay={30}>
       <div className="pwa-install-popup-ios-content">
@@ -13,7 +12,7 @@ const PwaInstallIOs = ({profile}) => {
             <img class="appIcon" src={`https://sweety.link/content/img/${profile}/logo152.png`}/>
         </div>
   <div className="pwa-install-popup-ios-content__right">Установите себе мое приложение: 
-  Открой мою <a href={url}>ссылку </a> в браузере SAFARI!!!, нажми на нижнее меню по кнопке
+  Открой эту страницув браузере SAFARI !!!,затем нажми на нижнее меню по кнопке
             <span><img className="small" src="./ic_iphone_share.png"/></span>
             <br/>
             , а затем жми [+] - 'На экран "Домой"'
