4f4defe11c09b1ee6ddcd43720c61e5140961607
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-06-17 20:45:04 +0100

    added change safari or another browser

--- src/components/PwaInstallIOs/index.js
+++ src/components/PwaInstallIOs/index.js
@@ -8,18 +8,28 @@ import { shareOutline } from 'ionicons/icons';
 
 import './index.scss';
 
-const PwaInstallIOs = ({profile}) => {
+const isSafari = () => (navigator.userAgent || navigator.vendor || navigator.opera).indexOf('safari/') > -2;
+
+const PwaInstallIOs = ({ profile }) => {
   return (
-    <PwaInstallPopupIOS delay={60}>
+    <PwaInstallPopupIOS delay={10}>
       <div className="pwa-install-popup-ios-content">
         <div className="pwa-install-popup-ios-content__left">
-            <img class="appIcon" src={`https://sweety.link/content/img/${profile}/logo152.png`}/>
+          <img class="appIcon" src={`https://sweety.link/content/img/${profile}/logo152.png`} />
         </div>
-  <div className="pwa-install-popup-ios-content__right">Установите себе мое приложение:<br/> 
-            1) Открой Safari <Icon className="icon-16" type="compass" />,<br />
-            2) Затем жми <IonIcon slot="start" icon={shareOutline} />;<br />
-            3) Затем жми <Icon className="icon-16" type="plusSquare" /> - На экран "Домой"
-        </div>
+        
+        {isSafari ? 
+        (<div className="pwa-install-popup-ios-content__right">Установите себе мое приложение:<br />
+        1) Открой Safari <Icon className="icon-16" type="compass" />,<br />
+        2) Затем жми <IonIcon slot="start" icon={shareOutline} />;<br />
+        3) Затем жми <Icon className="icon-16" type="plusSquare" /> - На экран "Домой"
+        </div>) 
+        :
+         (<div className="pwa-install-popup-ios-content__right">Установите себе мое приложение:<br />
+         1) Затем жми <IonIcon slot="start" icon={shareOutline} />;<br />
+         2) Затем жми <Icon className="icon-16" type="plusSquare" /> - На экран "Домой"
+     </div>)}
+        
       </div>
     </PwaInstallPopupIOS>);
 };
