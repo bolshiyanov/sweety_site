77a9d14df9b47e990d39d173684b751ef8d7742f
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-07-02 23:53:43 +0100

    new app texts landing

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -33,12 +33,14 @@ const Landing = () => {
     setStartOpened(false);
   }, [setStartOpened]);
 
+  const startDashUrl= 'https://dash.sweety.link'
+
   const openStart = useCallback(() => {
     setStartOpened(true);
   }, [setStartOpened]);
 
   const referrer = {
-    title: "Создай свое приложение",
+    title: "Новая эра приложений",
     avatar: referrerAvatar
   }
 
@@ -151,30 +153,62 @@ const Landing = () => {
         <Swiper getSwiper={setSwiper}>
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage1})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Активная ссылка<br />в профиле твоих<br />Соцсетей!</h1>
-              <p>ТВОЕ PWA ПРИЛОЖЕНИЕ...</p>
-
+              <h1 className="main-page__page1__header">6 ВЕСКИХ ПРИЧИН СДЕЛАТЬ СВОЕ ПРИЛОЖЕНИЕ ЗА 4999 руб! </h1>
+              <p>SWEETY VS ТАПЛИНКИ*</p>
             </div>
           </div>
+
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage2})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Бесплатно,<br />без регистрации,<br />восхитительно!</h1>
-
-              <p><a href="https://dash.sweety.link/" target="_blank">БЫСТРЫЙ СТАРТ ЗДЕСЬ...</a></p>
+              <h1 className="main-page__page1__header">НА ВИДУ- приложение можно увидеть на экране телефона в любое время!</h1>
+              <p>Ссылку на таплинки можно найти только в соцсетях! <br />Но как вспомниить аккаунт?</p>
             </div>
           </div>
+
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage3})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Добавь мессенджеры,<br />соцсети, YouTube и...<br />все, что угодно!</h1>
-              <p>ЗАРАБАТЫВАЙ ПРОЩЕ....</p>
+              <h1 className="main-page__page1__header">ДЛЯ "БЛОНДИНОК"- создавай приложение без дизайнеров и программистов!</h1>
+              <p>В таплинках конструирование тоже очень простое!</p>
             </div>
           </div>
+
           <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage4})` }}>
             <div className="main-page__page1__container1">
-              <h1 className="main-page__page1__header">Как микроблогеры использует Sweety</h1>
-              <p><a href="https://dash.sweety.link/" target="_blank">БЫСТРЫЙ СТАРТ ЗДЕСЬ</a></p>
+              <h1 className="main-page__page1__header">АВТОМАТИЗАЦИЯ- Sweety сам подкачивает обновления из YouTube и Instagram</h1>
+              <p>В таплинках есть админка. Но где найти ссылку и логин, который всегда теряется?</p>
               <div className="main-page__page1__container2">
+              </div>
+            </div>
+          </div>
 
+          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage5})` }}>
+            <div className="main-page__page1__container1">
+              <h1 className="main-page__page1__header">ПРОЗРАЧНО- Sweety можно купить и получить на флешке*. После установки работает 
+              без интернета!</h1>
+              <p>За таплинки нужно платить постоянно! И сколько будет в итоге оплачено за "чужую" ссылочку?</p>
+              <div className="main-page__page1__container2">
+              </div>
+            </div>
+          </div>
+
+          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage6})` }}>
+            <div className="main-page__page1__container1">
+              <h1 className="main-page__page1__header">СЕРЬЕЗНЫЙ БИЗНЕС ИМЕЕТ СВОЕ ПРИЛОЖЕНИЕ!</h1>
+              <p>Таплинки-  это полу сайт и только!</p>
+              <div className="main-page__page1__container2">
+              </div>
+            </div>
+          </div>
+
+          <div onClick={handleClick} className="main-page__page1" style={{ backgroundImage: `URL(${backgroundImage7})` }}>
+            <div className="main-page__page1__container1">
+              <h1 className="main-page__page1__header">БЕСПЛАТНАЯ РЕКЛАМА- В приложениях можно отправлять PUSH уведомления прямо в телефон!</h1>
+              <p>Реклама с посадкой на таплинки может быть только платной!</p>
+              <br/>
+              <p>*Таплинки: под таплинками понимается любой сервис быстрых ссылок для социальных сетей</p>
+              <p>*Получить на флэшке:  В любое время после окончания настроек приложения вы можете, заказать исходный код вашего приложения. </p>
+              <p>*Работает без интернета: Если пользователь открывал ваше приложение хотя бы один раз, оно сохраняется в кэше телефона.</p>
+              <div className="main-page__page1__container2">
               </div>
             </div>
           </div>
@@ -183,26 +217,35 @@ const Landing = () => {
             <Loading />
           </div>
         </Swiper>
-        
-          <div className="main-page__footer">
-            <div className="main-page__pages">
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 0 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 1 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 2 })} />
-              <div className={classnames({ 'main-page__pages__selected': currentPage === 3 })} />
-            </div>
-            <div className="cookie-box" >
-              <CookieBanner styles={styles}
-                message='Мы используем Cookies для Goole analitics. Мы не собираем персональные данные'
-                buttonMessage='Закрыть'
-                link={<a href='https://ru.wikipedia.org/wiki/Cookie' target="_blank">Что это: COOKIES</a>}
-              />
-            </div>
-            {!startOpened && currentPage > 2 && (
-            <Start />
-            )}
+
+        <div className="main-page__footer">
+          <div className="main-page__pages">
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 0 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 1 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 2 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 3 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 4 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 5 })} />
+            <div className={classnames({ 'main-page__pages__selected': currentPage === 6 })} />
+
           </div>
-        
+          <div className="cookie-box" >
+            <CookieBanner styles={styles}
+              message='Мы используем Cookies для Goole analitics. Мы не собираем персональные данные'
+              buttonMessage='Закрыть'
+              link={<a href='https://ru.wikipedia.org/wiki/Cookie' target="_blank">Что это: COOKIES</a>}
+            />
+          </div>
+          {!startOpened && currentPage > 5 && (
+            <Start />
+          )}
+           {!startOpened && currentPage < 6 && (
+          <Button name="start" className="story-settings__preview__button" onClick={() => window.open(startDashUrl)} >Попробовать бесплатно</Button>
+          )}
+          
+          <div className="textlogolanding">&reg;IMEC&nbsp;2015-2020</div>
+        </div>
+
       </div>
 
     </React.Fragment>
