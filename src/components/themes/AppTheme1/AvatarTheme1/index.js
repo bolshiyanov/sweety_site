4c439effb7f8e7e447d7239869c286e65d9f26e9
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-07-09 22:53:09 +0100

    detectMobile

--- src/components/themes/AppTheme1/AvatarTheme1/index.js
+++ src/components/themes/AppTheme1/AvatarTheme1/index.js
@@ -8,30 +8,36 @@ import Button from 'components/common/Button';
 import './index.scss';
 
 const AvatarTheme1 = () => {
-  const [data, setData] = useState({ name: '', avatar: ''});
+  const [data, setData] = useState({ name: '', avatar: '' });
 
-  const {  name, avatar, url } = useSelector((state) => state.config.data);
-  
+  const detectMobile = () => {
+    const toMatch = [/Android/i, /webOS/i, /iPhone/i, /iPad/i, /iPod/i,
+      /BlackBerry/i, /Windows Phone/i]; return toMatch.some((toMatchItem) => { return navigator.userAgent.match(toMatchItem); });
+  }
+
+  const { name, avatar, url } = useSelector((state) => state.config.data);
+
   const onShare = () => {
     navigator.share({
-        title: {name}, // Заголовок
-        text: 'Установи мое приложение по этой ссылке', // Текст
-        url: {url}, // ссылка
-      });
-    };
-  
-    return (
-      <React.Fragment>
-      <div className="user-name-theme1"  >{name || "Theme 1"}&nbsp;: Official App&nbsp;
-      {navigator.share && 
-            <Button onClick={onShare} isInline className="tooltip-theme1">
-          <IonIcon slot="start" icon={shareOutline} /></Button>}
+      title: { name }, // Заголовок
+      text: 'Установи мое приложение по этой ссылке', // Текст
+      url: { url }, // ссылка
+    });
+  };
 
-      
-      </div>    
+  return (
+    <React.Fragment>
+      <div className="user-name-theme1"  >{name || "Theme 1"}
+        {detectMobile() ? " : Official App" : " : Official Website"}
+        {navigator.share &&
+          <Button onClick={onShare} isInline className="tooltip-theme1">
+            <IonIcon slot="start" icon={shareOutline} /></Button>}
+
+
+      </div>
       <div className="avatar-theme1"
-       style={{ backgroundImage: `URL(${data.avatar || addedAvatar})` }}  />
-       </React.Fragment>
+        style={{ backgroundImage: `URL(${data.avatar || addedAvatar})` }} />
+    </React.Fragment>
   );
 };
 
