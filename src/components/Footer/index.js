4c0a321c285dd8c546d9f552319bd28ff526badc
test <test@user.com>
2020-09-08 15:22:15 +0300

    Refactor code in new app

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { IonIcon } from '@ionic/react';
-import { logoPwa, logoApple, logoAndroid, logoWindows } from 'ionicons/icons';
 
 import './index.scss';
 
 const Footer = () => {
-  const startUrl = 'https://sweety.link'
-  const { url, name } = useSelector((state) => state.config.config.whiteLabel);
+  const { url } = useSelector((state) => state.config.config.whiteLabel);
   const { active } = useSelector((state) => state.config.account);
-  const { currentTheme } = useSelector((state) => state.config);
-  const fontStyles = currentTheme.getFontColorStyles();
+
   return (
     <footer>
       {active ? <br /> : <a href={url} target="_blank" className="textfooter">Создай приложение как у меня</a>}
