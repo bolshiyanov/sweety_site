a87e2da74db0b8994dc643a3d03095c93948ac00
test <test@user.com>
2020-09-08 18:56:35 +0300

    Fix video avatar for non-ios

--- src/components/common/AvatarBase/index.js
+++ src/components/common/AvatarBase/index.js
@@ -7,26 +7,28 @@ const AvatarBase = ({
     avatar, 
     avatarPreview, 
     avatarDefault, 
-    needPreview,
     wrapperImageClass, 
     wrapperVideoClass 
 }) => {
-  const [videoAvatar, setVideoAvatar] = useState(true);
+  const [isVideoAvatar, setIsVideoAvatar] = useState(avatar?.endsWith(".mp4") ?? false);
 
   const detectIOs = () => {
-    const toMatch = [/iPhone/i, /iPad/i, /iPod/i, /iMac/i]; return toMatch.some((toMatchItem) => { return navigator.userAgent.match(toMatchItem); });
-  }
+    const toMatch = [/iPhone/i, /iPad/i, /iPod/i, /iMac/i]; 
+    return toMatch.some((toMatchItem) => { return navigator.userAgent.match(toMatchItem); });
+  };
+
+  const isIOs = detectIOs();
 
   return <React.Fragment>
-    {detectIOs && avatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarDefault})` }} />}
+    {isIOs && avatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarDefault})` }} />}
     
-    {!detectIOs && avatarPreview && !needPreview && videoAvatar && <div className={wrapperVideoClass}>
+    {!isIOs && isVideoAvatar && <div className={wrapperVideoClass}>
       <video className="avatar-video-base" poster={avatarPreview} preload="auto" autoplay="true" loop="true" muted="muted">
-        <source src={avatar} onError={() => setVideoAvatar(false)}></source>
+        <source src={avatar} onError={() => setIsVideoAvatar(false)}></source>
       </video>
     </div>}
-    {!detectIOs && avatarPreview && (needPreview || !videoAvatar) && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarPreview}), URL(${avatarDefault})` }} />}
-    {!detectIOs && !avatarPreview && avatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarDefault})` }} />}
+    {!isIOs && avatarPreview && !isVideoAvatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarPreview}), URL(${avatarDefault})` }} />}
+    {!isIOs && !avatarPreview && avatar && !isVideoAvatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatar}), URL(${avatarDefault})` }} />}
     
     {!avatar && <div className={wrapperImageClass} style={{ backgroundImage: `URL(${avatarDefault})` }} />}
   </React.Fragment>
@@ -41,8 +43,7 @@ AvatarBase.propTypes = {
 AvatarBase.defaultProps = {
   avatar: null,
   avatarPreview: null,
-  avatarDefault: null,
-  needPreview: false
+  avatarDefault: null
 };
 
 export default AvatarBase; 
