d9a1a6eccf294595ef1084f1541e26ff1a8bb4e6
Roman Bolshiyanov <bolshiyanov@gmail.com>
2020-12-21 17:57:37 +0000

    Add new file

---
+++ src/components/GoogleAnalitycs/index.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
+
+const GoogleAnalitycs = () => {
+
+    // useEffect(() => {
+
+    //     ReactGA.initialize('UA-000000-01')
+    // }, [])
+
+    return (
+        <div>123456</div>
+    );
+}
+
+export default GoogleAnalitycs;
